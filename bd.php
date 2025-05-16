3839fe5bc259ac0c2479bca296d5f66dc4025cc5
Mélanie Gomis <melanie.gomis@etu.univ-montp3.fr>
2025-05-16 11:28:37 +0200

    Modification pour la BD test

--- bd.php
+++ bd.php
@@ -1,12 +1,14 @@
 <?php
 $host = 'localhost';
-$dbname = 'nom_de_la_base';
+$dbname = 'test';
 $username = 'root';
 $password = 'root';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Connexion réussie à la base de données.";
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
