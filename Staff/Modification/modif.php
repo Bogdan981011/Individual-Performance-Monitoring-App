44e019aa72332f20b21a3790f62b08fbbbcfa0c0
Mélanie Gomis <melanie.gomis@etu.univ-montp3.fr>
2025-05-22 11:03:27 +0200

    Correction de la connexion joueur

--- Staff/Modification/modif.php
+++ Staff/Modification/modif.php
@@ -12,36 +12,46 @@ session_start();
 <body>
 
 <div class="form-container">
-    <h1>Ajouter un joueur</h1>
+    <?php
+    include_once "../../bd.php";
+
+    try{
+    $id_joueur = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    if ($id_joueur === false) {
+        echo "<p>Une erreur est survenue. Redirection...</p>";
+        echo "<script>setTimeout(() => window.location.href = '../../accueil_staff.html', 1000);</script>";
+        exit;
+    }
+
+    $stmt = $pdo->prepare("SELECT * FROM joueur WHERE id_joueur = :id");
+    $stmt -> execute(['id' => $id_joueur]);
+    $joueur = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $prenom = htmlspecialchars($joueur['prenom']);
+    $nom = htmlspecialchars($joueur['nom']);
+    $naissance = htmlspecialchars($joueur['annee']);
+    $poste = htmlspecialchars($joueur['poste']);
+    $email = htmlspecialchars($joueur['email']);
+    $id_eq = htmlspecialchars($joueur['id_equipe']);
+
+    $stmt = $pdo->prepare("SELECT nom_equipe FROM equipe WHERE id_equipe = :id");
+    $stmt -> execute(['id' => $id_eq]);
+    $equipe = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $equipe = htmlspecialchars($equipe['nom_equipe']);
+
+    } catch (PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+    }
+    ?>
+    <h1><?= "Modification du profil de " . $prenom. " " . 'nom' ?></h1>
+    <p> Merci de ne remplir que les champs à modifier.</p>
 
     <form method="post" action="ajouter_joueur.php">
-        <?php
-        include_once "../../../bd.php";
-
-        try{
-        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-        if ($id === false) {
-          echo "<p>Une erreur est survenue. Redirection...</p>";
-          echo "<script>setTimeout(() => window.location.href = '../../accueil_staff.html', 1000);</script>";
-          exit;
-        }
-
-        $stmt = $pdo->prepare("SELECT * FROM joueur WHERE id=:id");
-        $stmt -> execute(['id' => $id_joueur]);
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        ?>
-        <h2><?= htmlspecialchars($result['prenom'] . " " . $result['nom']) ?></h2>
-        <?php
-
-      } catch (PDOException $e){
-        echo "Erreur : " . $e->getMessage();
-      }
-        ?>
         <p>
             <label for="id_equipe">Équipe :</label>
             <select name="id_equipe" id="id_equipe">
-                <option value="">-- Sélectionnez une équipe --</option>
+                <option value="" disabled selected> <?= $equipe ?> </option>
                 <option value="crabos">CRABOS</option>
                 <option value="cadets a">CADETS A</option>
                 <option value="cadets b">CADETS B</option>
