1959743615489c8ca315738472da512e7f00d978
Mélanie Gomis <melanie.gomis@etu.univ-montp3.fr>
2025-05-23 10:48:51 +0200

    Correction affiche section test

---
+++ Joueur/Modification/modif.php
@@ -0,0 +1,46 @@
+<?php 
+session_start(); 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Modifier un joueur</title>
+    <link rel="stylesheet" href="../Styles/nouveau.css">
+    <script src="modif.js"></script>
+</head>
+<body>
+    <b><a href="../accueil_joueur.html">Retour</a></b>
+
+<div class="form-container">
+    
+    <h1>Modification du mot de passe</h1>
+
+    <form>
+      <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
+
+        <!-- Mot de passe -->
+        <p>
+            <label for="mdp_av">Ancien mot de passe:</label>
+            <input type="password" name="mdp_av" id="mdp_av">
+        </p>
+
+        <p>
+            <label for="mdp">Nouveau mot de passe :</label>
+            <input type="password" name="mdp" id="mdp">
+        </p>
+
+        <p>
+            <label for="confirmation_mdp">Confirmation du nouveau mot de passe :</label>
+            <input type="password" name="confirmation_mdp" id="confirmation_mdp">
+        </p>
+
+        <p>
+            <input type="submit" value="Ajouter">
+        </p>
+    </form>
+</div>
+
+</body>
+</html>
